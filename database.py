8ef0a73a6d4acd6dfed1b3354946e02cd0407c86
aliimrantec <ali.imran.tec@gmail.com>
2026-07-01 13:40:17 +0500

    Create jobs table in SQLite database

--- database.py
+++ database.py
@@ -26,6 +26,16 @@ def initialize_database():
             password TEXT NOT NULL
         )
     """)
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS jobs (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            company TEXT NOT NULL,
+            location TEXT,
+            job_url TEXT,
+            posted_time TEXT
+        )
+    """)
 
     connection.commit()
     connection.close()
