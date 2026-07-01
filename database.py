e3906289a5416ca687089b27d63dc80ae72e2b4a
aliimrantec <ali.imran.tec@gmail.com>
2026-07-01 10:08:42 +0500

    initialize sqllite databasw

--- database.py
+++ database.py
@@ -1,8 +1,23 @@
 import sqlite3
 from config import Config
+
 def get_connection():
     """
     Create and return a connection to the SQLite database.
     """
     connection = sqlite3.connect(Config.DATABASE_PATH)
     return connection
+
+
+def initialize_database():
+    """
+    initializing the sqlite database ...
+    """
+    
+    connection=get_connection()
+    print("database initialized successfully")
+    connection.close()
+    
+if __name__ == "__main__":
+    initialize_database()
+    
